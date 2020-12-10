2660e5fa73d08cce92711964888f25a7a96b625d
老广 <ibuler@qq.com>
2020-12-10 20:41:54 +0800

    Fix master (#8)
    * fix: 修改生成的默认config
    
    * perf: 修改运行
    
    * fix: 修改名称

--- jmsctl.sh
+++ jmsctl.sh
@@ -105,7 +105,8 @@ function check_update() {
   echo "当前版本是: ${current_version}"
   echo
   confirm="no"
-  read_from_input confirm "要更新到这个版本吗?" "" "${confirm}"
+  read_from_input confirm "要更新到这个版本吗?" "yes/no" "${confirm}"
+  bash "${SCRIPT_DIR}/7_upgrade.sh" ${latest_version}"
 }
 
 function main() {
@@ -122,7 +123,7 @@ function main() {
     bash "${SCRIPT_DIR}/4_install_jumpserver.sh"
     ;;
   upgrade)
-    bash "${SCRIPT_DIR}/8_upgrade.sh" "$target"
+    bash "${SCRIPT_DIR}/7_upgrade.sh" "$target"
     ;;
   check_update)
     check_update
