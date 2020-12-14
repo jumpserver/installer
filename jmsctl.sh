d1d02218b3621f7f767206936bf9c23a2f61f21f
ibuler <ibuler@qq.com>
2020-12-14 21:26:02 +0800

    fix: 修复忘记添加的潘丹

--- jmsctl.sh
+++ jmsctl.sh
@@ -108,7 +108,9 @@ function check_update() {
   echo
   confirm="n"
   read_from_input confirm "要更新到这个版本吗?" "y/n" "${confirm}"
-  bash "${SCRIPT_DIR}/7_upgrade.sh" "${latest_version}"
+  if [[ "${confirm}" == "y" ]];then
+     bash "${SCRIPT_DIR}/7_upgrade.sh" "${latest_version}"
+  fi
 }
 
 function main() {
