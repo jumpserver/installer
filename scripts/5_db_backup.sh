0602384627373733b1340b5975382f6a40f1e018
wojiushixiaobai <296015668@qq.com>
2021-02-01 23:54:38 +0800

    perf: 优化脚本

--- scripts/5_db_backup.sh
+++ scripts/5_db_backup.sh
@@ -10,22 +10,21 @@ PORT=$(get_config DB_PORT)
 USER=$(get_config DB_USER)
 PASSWORD=$(get_config DB_PASSWORD)
 DATABASE=$(get_config DB_NAME)
-DB_FILE=${BACKUP_DIR}/jumpserver-$(date +%F_%T).sql
-DB_FILE_ZIP=${DB_FILE}.gz
+DB_FILE=${BACKUP_DIR}/${DATABASE}-$(date +%F_%T).sql
 
 function main() {
   mkdir -p ${BACKUP_DIR}
   echo "$(gettext 'Backing up')..."
   backup_cmd="mysqldump --host=${HOST} --port=${PORT} --user=${USER} --password=${PASSWORD} ${DATABASE}"
-  docker run --rm -i --network=jms_net jumpserver/mysql:5 ${backup_cmd} | gzip > ${DB_FILE_ZIP}
+  docker run --rm -i --network=jms_net jumpserver/mysql:5 ${backup_cmd} > ${DB_FILE}
 
   code="x$?"
   if [[ "$code" != "x0" ]]; then
     log_error "$(gettext 'Backup failed')!"
-    rm -f "${DB_FILE_ZIP}"
+    rm -f "${DB_FILE}"
     exit 1
   else
-    log_success "$(gettext 'Backup succeeded! The backup file has been saved to'): ${DB_FILE_ZIP}"
+    log_success "$(gettext 'Backup succeeded! The backup file has been saved to'): ${DB_FILE}"
   fi
 }
 
