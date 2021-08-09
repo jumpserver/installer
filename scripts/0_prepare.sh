565be504e4eeee1433b572c05a3a1bb9c2dcf7ed
wojiushixiaobai <296015668@qq.com>
2021-08-09 15:57:52 +0800

    fix: 修复离线包脚本

--- scripts/0_prepare.sh
+++ scripts/0_prepare.sh
@@ -71,7 +71,7 @@ function prepare_image_files() {
     pull_image "$image"
 
     filename=$(basename "${image}").tar
-    component=$(echo "${filename}" | awk -F: '{ print $1 }')
+    component=$(basename "${image}")
     md5_filename=$(basename "${image}").md5
     md5_path=${IMAGE_DIR}/${md5_filename}
 
