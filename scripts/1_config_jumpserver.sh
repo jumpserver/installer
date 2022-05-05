10469db255c05c03395c8bfc33bd46b4dfcf2249
ibuler <ibuler@qq.com>
2022-05-05 09:22:54 +0800

    fix: 修复设置外部数据库的bug

--- scripts/1_config_jumpserver.sh
+++ scripts/1_config_jumpserver.sh
@@ -59,7 +59,7 @@ function set_external_mysql() {
   mysql_user=$(get_config DB_USER)
   read_from_input mysql_user "$(gettext 'Please enter MySQL username')" "" "${mysql_user}"
   mysql_password=$(get_config DB_PASSWORD)
-  read_from_input mysql_pass "$(gettext 'Please enter MySQL password')" "" "${mysql_password}"
+  read_from_input mysql_password "$(gettext 'Please enter MySQL password')" "" "${mysql_password}"
   if ! test_mysql_connect "${mysql_host}" "${mysql_port}" "${mysql_user}" "${mysql_password}" "${mysql_db}"; then
     echo_red "$(gettext 'Failed to connect to database, please reset')"
     echo
