310f2c0ca96e7848670b7d5fceba5df3e8862749
吴小白 <296015668@qq.com>
2022-08-25 14:36:05 +0800

    feat: 更新 compose v2

--- scripts/const.sh
+++ scripts/const.sh
@@ -24,29 +24,27 @@ export OS=$(uname -s)
 export DOCKER_VERSION=20.10.17
 export DOCKER_MIRROR="https://download.jumpserver.org/docker/docker-ce/linux/static/stable"
 export DOCKER_BIN_URL="${DOCKER_MIRROR}/$(uname -m)/docker-${DOCKER_VERSION}.tgz"
-
-if [[ "$(uname -m)" == "aarch64" ]]; then
-  DOCKER_MD5=f9b6570a174df41aec6b822fba7a17aa
-fi
 if [[ "$(uname -m)" == "x86_64" ]]; then
   DOCKER_MD5=f8c950e9d4edb901c0a8124706f60919
+fi
+if [[ "$(uname -m)" == "aarch64" ]]; then
+  DOCKER_MD5=f9b6570a174df41aec6b822fba7a17aa
 fi
 if [[ "$(uname -m)" == "loongarch64" ]]; then
   DOCKER_MD5=6c6fc22839c1bbe3ce578c470c2cd719
 fi
 export DOCKER_MD5
 
-export DOCKER_COMPOSE_VERSION=1.29.2
+export DOCKER_COMPOSE_VERSION=v2.10.1
 export DOCKER_COMPOSE_MIRROR="https://download.jumpserver.org/docker/compose/releases/download"
 export DOCKER_COMPOSE_BIN_URL="${DOCKER_COMPOSE_MIRROR}/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)"
-
-if [[ "$(uname -m)" == "aarch64" ]]; then
-  DOCKER_COMPOSE_MD5=6477a1a275d5837106f1311a78876776
-fi
 if [[ "$(uname -m)" == "x86_64" ]]; then
-  DOCKER_COMPOSE_MD5=8f68ae5d2334eecb0ee50b809b5cec58
+  DOCKER_COMPOSE_MD5=494401b7b654b3aed2a3165c515be4f3
+fi
+if [[ "$(uname -m)" == "aarch64" ]]; then
+  DOCKER_COMPOSE_MD5=9d37678e4ea0595d790233b558d9efa1
 fi
 if [[ "$(uname -m)" == "loongarch64" ]]; then
-  DOCKER_COMPOSE_MD5=0b060d00fbc20f3f9c2f231e944db9b5
+  DOCKER_COMPOSE_MD5=25d2e756093dd7aa1581175c14e5c536
 fi
 export DOCKER_COMPOSE_MD5
