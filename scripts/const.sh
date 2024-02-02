83a5c5dce2c2589ec78740cd1fd6f95f16782d9e
吴小白 <296015668@qq.com>
2024-02-02 15:46:49 +0800

    feat: 更新 docker v25.0.3

--- scripts/const.sh
+++ scripts/const.sh
@@ -26,36 +26,36 @@ STATIC_ENV=${PROJECT_DIR}/static.env
 . "${STATIC_ENV}"
 
 export OS=$(uname -s)
-export DOCKER_VERSION=24.0.7
+export DOCKER_VERSION=25.0.3
 export DOCKER_MIRROR="https://download.jumpserver.org/docker/docker-ce/linux/static/stable"
 export DOCKER_BIN_URL="${DOCKER_MIRROR}/$(uname -m)/docker-${DOCKER_VERSION}.tgz"
 if [[ "$(uname -m)" == "x86_64" ]]; then
-  DOCKER_MD5=ad4add9b55f71c295e6f8f9e093eb53d
+  DOCKER_MD5=0ba827cc2a1bd3c4e61c424d81148be7
 fi
 if [[ "$(uname -m)" == "aarch64" ]]; then
-  DOCKER_MD5=89be712e6a933321362ced9a4d22baef
+  DOCKER_MD5=b103d07e8dffeb601128186f6aa0e714
 fi
 if [[ "$(uname -m)" == "loongarch64" ]]; then
-  DOCKER_MD5=124492bc9a838fb8e4917cac5833ceae
+  DOCKER_MD5=8eea6349b4fbda3fd9a0499ec7f03259
 fi
 if [[ "$(uname -m)" == "s390x" ]]; then
-  DOCKER_MD5=ca5586e1ba49cd5cab6fdcd03ac443c6
+  DOCKER_MD5=10d9d1a0c771ebcfbf078a3141d72154
 fi
 export DOCKER_MD5
 
-export DOCKER_COMPOSE_VERSION=v2.24.0
+export DOCKER_COMPOSE_VERSION=v2.24.5
 export DOCKER_COMPOSE_MIRROR="https://download.jumpserver.org/docker/compose/releases/download"
 export DOCKER_COMPOSE_BIN_URL="${DOCKER_COMPOSE_MIRROR}/${DOCKER_COMPOSE_VERSION}/docker-compose-linux-$(uname -m)"
 if [[ "$(uname -m)" == "x86_64" ]]; then
-  DOCKER_COMPOSE_MD5=fa754fec4f02baa1b7e7f8b1d2ad4a61
+  DOCKER_COMPOSE_MD5=aea273a344c430d1c89d23b21fa18f63
 fi
 if [[ "$(uname -m)" == "aarch64" ]]; then
-  DOCKER_COMPOSE_MD5=0c64adb54fac8e5ccd1fa1cecaca114f
+  DOCKER_COMPOSE_MD5=3202cc80ff6147d6eb823151c8889622
 fi
 if [[ "$(uname -m)" == "loongarch64" ]]; then
-  DOCKER_COMPOSE_MD5=0db3ab0a0e5d63165ab804356d1fe132
+  DOCKER_COMPOSE_MD5=a7a3372132ba4b241635b06eb74b2a45
 fi
 if [[ "$(uname -m)" == "s390x" ]]; then
-  DOCKER_COMPOSE_MD5=a4784eb9505581dce9df97c0b080e8ef
+  DOCKER_COMPOSE_MD5=53b35fe65a92175ce5dff8ee4e715df5
 fi
 export DOCKER_COMPOSE_MD5
