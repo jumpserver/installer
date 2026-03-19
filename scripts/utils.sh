7d64212a43bdc3bd465bcd713abc7c55501c26fb
ibuler <ibuler@qq.com>
2026-03-19 17:11:04 +0800

    perf: update osm

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -418,7 +418,7 @@ function get_docker_compose_cmd_line() {
     cmd+=" -f compose/web.https.yml"
   fi
 
-  if [[ ${HTTP_PORT} != "0"]];then
+  if [[ ${HTTP_PORT} != "0" ]];then
     cmd+=" -f compose/web.http.yml"
   fi
 
