a09e95892da47d33616fd9de79243f7a2546ff7b
feng <1304903146@qq.com>
2026-07-07 12:44:05 +0800

    perf: openbao

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -6,8 +6,8 @@ BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 . "${BASE_DIR}/const.sh"
 . "${BASE_DIR}/gists/common.sh"
 . "${BASE_DIR}/gists/conf.sh"
+. "${BASE_DIR}/gists/openbao.sh"
 . "${BASE_DIR}/gists/image.sh"
 . "${BASE_DIR}/gists/service.sh"
 
 namespace=${NAMESPACE:-jumpserver}
-
