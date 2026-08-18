9dddb5fb2c049295111fc7888100818d46e3cc71
Gerry.tan <qian.tan@fit2cloud.com>
2026-08-18 11:17:01 +0800

    perf: add default params for auto-login and tree-node-amount

--- scripts/7_upgrade.sh
+++ scripts/7_upgrade.sh
@@ -67,6 +67,8 @@ function upgrade_config() {
   check_and_set_config "JUMPSERVER_ENABLE_FONT_SMOOTHING" "true"
   check_and_set_config "USE_LB" "1"
   check_and_set_config "VERIFY_EXTERNAL_SSL" "false"
+  check_and_set_config "TREE_NODE_AMOUNT_ENABLED" "False"
+  check_and_set_config "AUTO_RELOGIN_AT_SESSION_EXPIRE" "True"
   # XPACK
   use_xpack=$(get_config_or_env USE_XPACK)
   if [[ "${use_xpack}" == "1" ]]; then
